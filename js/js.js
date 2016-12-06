4bf817bf0bc44048c40a305062551f4d1cb3b20f
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-12-06 11:18:23 +0100

    if conditions

--- js/js.js
+++ js/js.js
@@ -1,11 +1,20 @@
-function addNewItem(list) {
+function addNewItem(list, itemText) {
 	var listItem = document.createElement("li");
+
 	// listItem.innerText = "Hello";
+	listItem.innerText = itemText;
+
 	list.appendChild(listItem);
 }; 
 
 var newItem = document.getElementById('addBtn');
 
 newItem.onclick = function() {
-	addNewItem(document.getElementById("todoList"));
+	var itemText = prompt("What do you want to had to the list??");
+
+	if(!itemText || itemText == "" || itemText == " ") {
+		return false;
+	}
+
+	addNewItem(document.getElementById("todoList"), itemText);
 };
